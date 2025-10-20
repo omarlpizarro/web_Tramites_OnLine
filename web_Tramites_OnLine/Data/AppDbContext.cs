c436aa2634aeadf303826a171a9e77b4a9d960dd
omar <omar@DESKTOP-JE2MPBR>
2025-10-20 11:54:35 -0300

    Agregar archivos de proyecto.

---
+++ web_Tramites_OnLine/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace web_Tramites_OnLine.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //public DbSet<Documento> Documentos { get; set; }
+        //public DbSet<TipoDocumento> TiposDocumento { get; set; }
+    }
+
+}
